e45c91fc9f6799d61c63d84fb4c714d63e4df8d9
Kris-Simpson <the.same.kris@gmail.com>
2018-09-20 00:21:03 +0300

    add user settings add jobs saves

--- src/app.jsx
+++ src/app.jsx
@@ -2,17 +2,11 @@ import React from 'react';
 import JobsList from './components/jobs_list';
 import JobContent from './components/job_content';
 
-import Job from './models/job';
-
 export default class App extends React.Component {
   constructor(props) {
     super(props);
 
-    var job1 = new Job();
-    var job2 = new Job('Static job');
-
     this.state = {
-      jobs: [job1, job2],
       selectedJob: null
     }
 
@@ -30,7 +24,7 @@ export default class App extends React.Component {
       .container-fluid
         .row
           .col-2
-            JobsList(jobs=this.state.jobs, selectedJob=this.handleSelectedJob)
+            JobsList(selectedJob=this.handleSelectedJob)
 
           .col-10
             JobContent(job=this.state.selectedJob)
--- src/components/jobs_list.jsx
+++ src/components/jobs_list.jsx
@@ -1,17 +1,50 @@
 import React from 'react';
+import Job   from '../models/job';
+import Store from '../store';
+
+const store = new Store({
+  configName: 'user-preferences',
+  defaults: {
+    jobs: []
+  }
+});
 
 export default class JobsList extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      jobs: this.props.jobs
+      newJobName: '',
+      jobs: store.get('jobs')
     }
+
+    this.handleNewJobName = this.handleNewJobName.bind(this);
+    this.addJob = this.addJob.bind(this);
+  }
+
+  handleNewJobName(event) {
+    this.setState({
+      newJobName: event.target.value
+    });
+  }
+
+  addJob() {
+    var newJob = new Job(this.state.newJobName);
+    var jobs   = [...store.get('jobs')]; // duplicate obj
+    jobs.push(newJob);
+
+    store.set('jobs', jobs);
+    this.setState({
+      jobs: jobs
+    });
   }
 
   render() {
     return (pug`
       ul
+        li
+          input(onChange=this.handleNewJobName)
+          button(onClick=this.addJob) Add job
         each job, index in this.state.jobs
           li(key=index)
             button(onClick=this.props.selectedJob.bind(this, job))
